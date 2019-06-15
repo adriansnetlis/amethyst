059a2d200ead3986b8c4ff6735929af1244e0e50
Andrea Catania <info@andreacatania.com>
2019-06-15 16:14:26 +0200

    Added systems to sync the transforms and step the physics world

--- amethyst_phythyst/src/lib.rs
+++ amethyst_phythyst/src/lib.rs
@@ -1,16 +1,34 @@
 
+mod systems;
+
+use log::debug;
 use amethyst_core::{
     bundle::SystemBundle,
     shred::DispatcherBuilder,
 };
 use amethyst_error::Error;
 
+use crate::systems::{
+    PhysicsSyncTransformSystem,
+    PhysicsStepperSystem,
+};
+
+/// This bundle will register all required systems to handle the physics in your game.
+/// 
+/// This bundle will automatically place a barrier in the dispatcher, in this way
+/// allow you to control the systems that you want to execute before and after the
+/// syncrhonization phase just by registering them before or after this bundle.
+/// Usually this is the first bundle that you want to register.
 pub struct PhysicsBundle;
 
 impl<'a, 'b> SystemBundle<'a, 'b> for PhysicsBundle{
     fn build(self, builder: &mut DispatcherBuilder<'a, 'b>) -> Result<(), Error>{
         
-        println!("Physics bundle not yet registerd!");
+        builder.add(PhysicsSyncTransformSystem::new(), "physics_sync_transform_system", &[]);
+        builder.add_barrier();
+        builder.add(PhysicsStepperSystem::new(), "", &["physics_sync_transform_system"]);
+
+        debug!("Physics bundle registered.");
 
         Ok(())
     }
---
+++ amethyst_phythyst/src/systems/mod.rs
@@ -0,0 +1,6 @@
+
+mod physics_sync_transform_system;
+mod physics_stepper_system;
+
+pub use physics_sync_transform_system::PhysicsSyncTransformSystem;
+pub use physics_stepper_system::PhysicsStepperSystem;
