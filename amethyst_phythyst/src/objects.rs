2ec8cd41dbd77fc1b99c525f891670e1f0123f69
Andrea Catania <info@andreacatania.com>
2019-06-30 11:34:56 +0200

    Added area server

--- amethyst_phythyst/src/objects.rs
+++ amethyst_phythyst/src/objects.rs
@@ -1,66 +1,33 @@
 use amethyst_core::ecs::{Component, DenseVecStorage};
 
-/// PhysicsWorld is the opaque ID that identify a world in the physics server
-#[derive(Copy, Clone)]
-pub struct PhysicsWorldTag(pub std::num::NonZeroUsize);
+macro_rules! define_opaque_object{
+    ($what:ident, $doc_name:ident) => {
+         /// $what is the opaque ID that identify a `$doc_name` in the physics server
+        #[derive(Copy, Clone)]
+        pub struct $what(pub std::num::NonZeroUsize);
 
-/// Panic if called
-impl Default for PhysicsWorldTag {
-    fn default() -> Self {
-        panic!();
-        PhysicsWorldTag(std::num::NonZeroUsize::new(1).unwrap())
+        /// Panic if called
+        impl Default for $what {
+            fn default() -> Self {
+                panic!();
+                $what(std::num::NonZeroUsize::new(1).unwrap())
+            }
+        }
+
+        impl Component for $what {
+            type Storage = DenseVecStorage<$what>;
+        }
+
+        impl std::ops::Deref for $what {
+            type Target = std::num::NonZeroUsize;
+            fn deref(&self) -> &std::num::NonZeroUsize {
+                &self.0
+            }
+        }
     }
 }
 
-impl Component for PhysicsWorldTag {
-    type Storage = DenseVecStorage<PhysicsWorldTag>;
-}
-
-impl std::ops::Deref for PhysicsWorldTag {
-    type Target = std::num::NonZeroUsize;
-    fn deref(&self) -> &std::num::NonZeroUsize {
-        &self.0
-    }
-}
-
-/// PhysicsBody is the opaque ID that identify a body in the physics server
-#[derive(Copy, Clone)]
-pub struct PhysicsBodyTag(pub std::num::NonZeroUsize);
-
-/// Panic if called
-impl Default for PhysicsBodyTag {
-    fn default() -> Self {
-        panic!();
-        PhysicsBodyTag(std::num::NonZeroUsize::new(1).unwrap())
-    }
-}
-
-impl Component for PhysicsBodyTag {
-    type Storage = DenseVecStorage<PhysicsBodyTag>;
-}
-
-impl std::ops::Deref for PhysicsBodyTag {
-    type Target = std::num::NonZeroUsize;
-    fn deref(&self) -> &std::num::NonZeroUsize {
-        &self.0
-    }
-}
-
-/// Physics shape is the opaque ID that identify a body in the physics server
-#[derive(Copy, Clone)]
-pub struct PhysicsShapeTag(pub std::num::NonZeroUsize);
-
-/// Panic if called
-impl Default for PhysicsShapeTag {
-    fn default() -> Self {
-        panic!();
-        PhysicsShapeTag(std::num::NonZeroUsize::new(1).unwrap())
-    }
-}
-
-impl std::ops::Deref for PhysicsShapeTag {
-    type Target = std::num::NonZeroUsize;
-    fn deref(&self) -> &std::num::NonZeroUsize {
-        &self.0
-    }
-}
+define_opaque_object!(PhysicsWorldTag, World);
+define_opaque_object!(PhysicsBodyTag, Rigid_Body);
+define_opaque_object!(PhysicsAreaTag, Area);
+define_opaque_object!(PhysicsShapeTag, Shape);
