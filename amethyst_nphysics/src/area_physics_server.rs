2e25805b2a31bff69e48d3dd92519170097efd61
Andrea Catania <info@andreacatania.com>
2019-06-30 11:55:11 +0200

    Implementing sensor collider creation

--- amethyst_nphysics/src/area_physics_server.rs
+++ amethyst_nphysics/src/area_physics_server.rs
@@ -7,6 +7,14 @@ use amethyst_phythyst::{
     },
     objects::*,
 };
+use nphysics3d::{
+    object::{
+        Collider as NpCollider,
+        ColliderHandle as NpColliderHandle,
+        ColliderDesc as NpColliderDesc,
+    },
+    world::World as NpWorld,
+};
 use nalgebra::{
     RealField,
 };
@@ -30,6 +38,15 @@ impl<N: RealField> AreaPhysicsServerTrait for AreaNpServer<N> {
         world_tag: PhysicsWorldTag,
         area_desc: &AreaDesc,
     ) -> PhysicsAreaTag {
+        let mut worlds_storage = self.storages.worlds_w();
+        // area storage ?
+        let mut shapes_storage = self.storages.shapes_w();
+
+        let np_world = worlds_storage.get_mut(*world_tag).expect("During the area creation the world tag passed was not valid");
+        let shape = shapes_storage.get_mut(*area_desc.shape).expect("During area creation was not possible to find the shape");
+
+        let np_collider = NpColliderDesc::new(shape.shape_handle().clone()).sensor(true).build(np_world);
+
         PhysicsAreaTag::default()
     }
 
