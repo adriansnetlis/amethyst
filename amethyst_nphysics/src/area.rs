5dd6726e47dc1212dc0e30c9bbc7c381bf1739d4
Andrea Catania <info@andreacatania.com>
2019-07-01 09:03:46 +0200

    Implementing area

--- amethyst_nphysics/src/area.rs
+++ amethyst_nphysics/src/area.rs
@@ -6,13 +6,13 @@ use nphysics3d::object::{
 };
 
 pub struct Area{
-    pub collider_handle: NpColliderHandle,
+    pub collider_handle: Option<NpColliderHandle>,
     pub world_tag: PhysicsWorldTag,
     pub shape_tag: PhysicsShapeTag,
 }
 
 impl Area{
-    pub fn new(collider_handle: NpColliderHandle, world_tag: PhysicsWorldTag, shape_tag: PhysicsShapeTag) -> Self{
+    pub fn new(collider_handle: Option<NpColliderHandle>, world_tag: PhysicsWorldTag, shape_tag: PhysicsShapeTag) -> Self{
         Area{
             collider_handle,
             world_tag,
